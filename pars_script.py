2bb21a88967767f36c49cb79720bb8a26e7d7450
Grino777 <bars_B_P@mail.ru>
2022-08-29 13:53:30 +0300

    Update project

--- pars_script.py
+++ pars_script.py
@@ -1,36 +1,23 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-
+from html_json import parsing_data_from_html
 
 
 def get_template_page(url):
-    # browser_options = Options()
-    # browser_options.add_argument('--headless')
-    # browser_options.add_argument("--disable-notifications")
-
     driver = webdriver.Chrome(executable_path='webdriver/chromedriver.exe')
-    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    # driver.maximize_window()
-
     try:
         driver.get(url=url)
-        sleep(5)
+        # sleep(5)
 
         while True:
-            # find_end_element = driver.find_element(By.TAG_NAME, 'script')
             find_end_element = driver.find_element(By.CLASS_NAME, 'footer-navigation')
 
             if driver.find_element(By.CLASS_NAME, 'listview-band-bottom'):
-                # with open('templates/0.html', 'w', encoding='utf-8') as file:
-                    # file.write(find_end_element)
-                    # file.write(driver.page_source)
-                break
-
+                html_page_code = driver.page_source
+                return parsing_data_from_html(html_page_code)
             else:
                 actions = ActionChains(driver)
                 actions.move_to_element(find_end_element).perform()
@@ -45,7 +32,9 @@ def get_template_page(url):
 
 def main():
     url = 'https://www.wowhead.com/wotlk/ru/items#items;0'
-    get_template_page(url)
+    all_items = {}
+    all_items.update(get_template_page(url))
+    print(all_items)
 
 
 if __name__ == '__main__':
